965d1fc0c43b362a81a4a8a2d4bbac3f2d0231d7
AndreManik <andre.ivan.manik@gmail.com>
2017-07-26 22:42:50 +0300

    Add files via upload

---
+++ ad/Advertisement.java
@@ -0,0 +1,21 @@
+package com.javarush.task.task27.task2712.ad;
+
+public class Advertisement {
+
+    private Object content;         // видео
+    private String name;            // имя/название
+    private long initialAmount;     // начальная сумма, стоимость рекламы в копейках. Используем long, чтобы избежать проблем с округлением
+    private int hits;               // количество оплаченных показов
+    private int duration;           // продолжительность в секундах
+
+
+    //В классе Advertisement создадим конструктор с порядком аргументов,
+    // соответствующим перечисленной последовательности всех полей класса
+    public Advertisement(Object content, String name, long initialAmount, int hits, int duration) {
+        this.content = content;
+        this.name = name;
+        this.initialAmount = initialAmount;
+        this.hits = hits;
+        this.duration = duration;
+    }
+}
---
+++ ad/AdvertisementStorage.java
@@ -0,0 +1,52 @@
+package com.javarush.task.task27.task2712.ad;
+
+// хранилище рекламных роликов.
+// Так как хранилище рекламных роликов AdvertisementStorage
+// единственное для всего ресторана, то сделаем его синглтоном.
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AdvertisementStorage {
+
+    /*
+        Видео должно где-то храниться, пусть это будет список.
+        Создадим поле videos и инициализируем его пустым листом.
+    */
+
+    private final List<Advertisement> videos = new ArrayList<>();
+
+    private static AdvertisementStorage storage;
+
+    public static AdvertisementStorage getInstance(){
+
+        if (storage == null){
+            storage = new AdvertisementStorage();
+        }
+
+        return storage;
+    }
+
+    //Чтобы как-то работать с видео, создай публичные методы:
+    //list() — который вернет список всех существующих доступных видео.
+    public List<Advertisement> list(){
+        return this.videos;
+    }
+
+    //add(Advertisement advertisement) — который добавит новое видео в список videos
+    public void add(Advertisement advertisement){
+        videos.add(advertisement);
+    }
+
+
+    private AdvertisementStorage(){
+
+        //В конструкторе класса добавим в список videos какие-то данные. У меня это:
+        Object someContent = new Object();
+        new Advertisement(someContent, "First Video", 5000, 100, 3 * 60); // 3 min
+        new Advertisement(someContent, "Second Video", 100, 10, 15 * 60); //15 min
+        new Advertisement(someContent, "Third Video", 400, 2, 10 * 60); //10 min
+
+    }
+
+}
